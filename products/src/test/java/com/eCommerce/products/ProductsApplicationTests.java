4e4525827b7b1aa88291dfc8246a67512cc3b408
Adarsh srivathsa M S <adarshsrivathsa1642000@gmail.com>
2024-03-03 16:00:03 +0530

    Server port changed and Added Integration Test for mongo DS

--- products/src/test/java/com/eCommerce/products/ProductsApplicationTests.java
+++ products/src/test/java/com/eCommerce/products/ProductsApplicationTests.java
@@ -1,13 +1,38 @@
 package com.eCommerce.products;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ProductsApplicationTests {
 
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateObject() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+			.contentType(MediaType.APPLICATION_JSON)
+			
+		);
 	}
 
 }
