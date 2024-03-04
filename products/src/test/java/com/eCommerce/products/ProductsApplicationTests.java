cdd32e424782e1abf730208088a10b264481073b
Adarsh srivathsa M S <adarshsrivathsa1642000@gmail.com>
2024-03-04 16:42:12 +0530

    Integration test of DI mongo

--- products/src/test/java/com/eCommerce/products/ProductsApplicationTests.java
+++ products/src/test/java/com/eCommerce/products/ProductsApplicationTests.java
@@ -1,7 +1,11 @@
 package com.eCommerce.products;
 
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -12,15 +16,28 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import com.eCommerce.products.dto.ProductRequest;
+import com.eCommerce.products.repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
 class ProductsApplicationTests {
 
 	@Container
-	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0");
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private ProductRepository productRepository;
 
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
@@ -29,10 +46,27 @@ class ProductsApplicationTests {
 
 	@Test
 	void shouldCreateObject() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+
 		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
-			.contentType(MediaType.APPLICATION_JSON)
-			
-		);
+							.contentType(MediaType.APPLICATION_JSON)
+							.content(productRequestString))
+						.andExpect(status().isCreated());
+
+
+		Assertions.assertEquals(1,productRepository.findAll().size());
+	}
+
+	private ProductRequest getProductRequest() {
+		// TODO Auto-generated method stub
+
+		return ProductRequest.builder()
+			.name("moto g40")
+			.description("Moto smartphone")
+			.price(BigDecimal.valueOf(1200))
+			.build();
+	
 	}
 
 }
