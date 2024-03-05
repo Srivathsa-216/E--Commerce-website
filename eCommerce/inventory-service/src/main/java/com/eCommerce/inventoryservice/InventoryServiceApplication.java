85496a312d34c2e03907a6c13e078f7e4f94e96f
Adarsh srivathsa M S <adarshsrivathsa1642000@gmail.com>
2024-03-05 16:13:14 +0530

    Added find SKU functionality through Inventory service

--- eCommerce/inventory-service/src/main/java/com/eCommerce/inventoryservice/InventoryServiceApplication.java
+++ eCommerce/inventory-service/src/main/java/com/eCommerce/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,36 @@
 package com.eCommerce.inventoryservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.eCommerce.inventoryservice.model.Inventory;
+import com.eCommerce.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("Samsung S21");
+			inventory.setQuantity(51);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("Samsung S20");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+
+
+		};
 	}
 
 }
