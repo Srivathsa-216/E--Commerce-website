705dd70c981dfe775138c1bcf4decdc50b9a8934
Adarsh srivathsa M S <adarshsrivathsa1642000@gmail.com>
2024-03-05 13:08:50 +0530

    Adding Inventory service, prevly completed the test order placement

--- eCommerce/order-service/src/main/java/com/eCommerce/orderservice/controller/OrderController.java
+++ eCommerce/order-service/src/main/java/com/eCommerce/orderservice/controller/OrderController.java
@@ -1,22 +1,31 @@
 package com.eCommerce.orderservice.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eCommerce.orderservice.dto.OrderRequest;
+import com.eCommerce.orderservice.service.OrderService;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 @RequestMapping("/api/order/")
+@RequiredArgsConstructor
 public class OrderController {
 
+    private final OrderService orderService;
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        
-        return "Order Placed Successfully";
+        orderService.placeOrder(orderRequest);
+        return "Order placed Successfuly";
     }
 
 
