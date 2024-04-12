886cc699e77a4abf98a9a9981b8ea9e1764be275
Adarsh srivathsa M S <srivathsa216@gmail.com>
2024-04-12 20:38:44 +0530

    Implementing for Order service

--- eCommerce/order-service/src/main/java/com/eCommerce/orderservice/service/OrderService.java
+++ eCommerce/order-service/src/main/java/com/eCommerce/orderservice/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -25,6 +26,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final KafkaTemplate kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -55,6 +57,7 @@ public class OrderService {
         
         if( allProductsInStock ){
             orderRepository.save(order);
+            kafkaTemplate.send("NotifactionTopic", order.getOrderNumber());
             return "Order Placed Successfuly";
         }
         else{
